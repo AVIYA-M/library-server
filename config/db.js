81263ca240086d084aa7c6969c21dbfa1f4a0d9a
aviya matzliach <aviya1103@gmail.com>
2026-07-23 16:53:54 +0300

    resolve SRV lookup using google DNS and move secrets to env

--- config/db.js
+++ config/db.js
@@ -1,15 +1,11 @@
-import dns from 'dns';
 import mongoose from 'mongoose';
-
-
-dns.setDefaultResultOrder('ipv4first');
+import dotenv from 'dotenv';
+import dns from 'dns';
 dns.setServers(['8.8.8.8', '8.8.4.4']);
-
-const MONGO_URI = 'mongodb+srv://AVIYA-M:avi33095@cluster0.18udexl.mongodb.net/?appName=Cluster0';
-
+dotenv.config();
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected successfully!');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
